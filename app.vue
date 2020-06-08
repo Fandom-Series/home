e03bb1b58a43ddc9f8f7bca6fa612ad0847d8626
Fandom-Series <53364190+Fandom-Series@users.noreply.github.com>
2020-06-08 13:58:08 -0600

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,29 @@
+<div id="app">
+  <html>
+	<head>
+		<title>Peliculas</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+			<link rel="stylesheet" href="assets/css/main.css" />
+    		<link rel="stylesheet" href="assets/css/slider.css" />
+    	
+                 <link rel="stylesheet" href="assets/css/busqui.css" />
+		<link rel="stylesheet" href="loadin.css">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+  
+	</head>
+  <div class="search-wrapper">
+    <input type="text" v-model="search" placeholder="Search title.."/>
+        <label>Search title:</label>
+  </div>
+  <div class="wrapper">
+    <div class="card" v-for="post in filteredList">
+      <a v-bind:href="post.link" target="_blank">
+        <img v-bind:src="post.img"/>
+        <small>Año: {{ post.author }}</small>
+        {{ post.title }},
+        {{ post.lang }}
+      </a>
+    </div>
+  </div>
+</div>
